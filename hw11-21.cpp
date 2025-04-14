7f194a13f74f2dbe0dccd4b7009e1be5d0b9a31c
Tyler Sharer <tylersharer@my.tridenttech.edu>
2025-04-14 16:01:40 -0400

    Submission for chapter 11. Tyler Sharer

--- hw11-21.cpp
+++ hw11-21.cpp
@@ -1,17 +1,30 @@
 //Introductory21.cpp - Displays the average stock price
-//Created/revised by <your name> on <current date>
+//Created/revised by <Tyler Sharer> on <10/25/2023>
+//Program that calculates and displays the average stock price from the prices array.
 
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
+//function to find average
+double findAverage(double prices[], int SIZE) {
+	double total = 0.0; //initialize total to 0
+	for (int i = 0; i < SIZE; ++i) { //loop through array
+		total += prices[i]; //add current element to total
+	}
+	return total / SIZE; //return average
+}
 int main()
 {
+	//Array function prototypes
+	int SIZE = 10; //size of array
 	double prices[10] = {96.5, 100.5, 100.5, 100.5, 99, 99, 99, 100, 98.5, 98.9};
-	double total = 0.0;
 	double average = 0.0;
 
+	//call function
+	average = findAverage(prices, SIZE); //store average price
 
+	//output results
 	cout << fixed << setprecision(2);
 	cout << "Average stock price: $" << average << endl;
 	return 0;
--- lab11-2.cpp
+++ lab11-2.cpp
@@ -1,13 +1,60 @@
 //Lab 9-2.cpp - displays two monthly car payments
-//Created/revised by <your name> on <current date>
-
+//Created/revised by <Tyler Sharer> on <10/25/2023>
+//This program stores (5) race finish times from from the user and stores in an array. It uses two value returning functions to calculates the average time and lowest time.
 #include <iostream>
 #include <cmath>
 #include <iomanip>
 using namespace std;
 
+//array
+double finishTimes[5]; //array to hold finish times
+const int SIZE = 5; //size of array
+
+//find lowest function
+double findLowest(double finishTimes[], double SIZE) {
+	double lowest = finishTimes[0]; //initialize lowest to first element
+	for (int i = 1; i < SIZE; ++i) { //loop through array
+		if (finishTimes[i] < lowest) { //if current element is less than lowest
+			lowest = finishTimes[i]; //set lowest to current element
+		}
+	}
+	return lowest; //return lowest value
+}
+double findAverage(double finishTimes[], double SIZE) {
+	double total = 0.0; //initialize total to 0
+	for (int i = 0; i < SIZE; ++i) { //loop through array
+		total += finishTimes[i]; //add current element to total
+	}
+	return total / SIZE; //return average
+}
 int main()
 {
+	//Array function prototypes
+	
+	
+	//Loop to fill array
+	for (int i = 0; i < SIZE; ++i) {
+		cout << "Enter time " << i + 1 << ": ";
+		cin >> finishTimes[i];
+	}
+	//variable declarations
+	const int SIZE = 5; //size of array
+	double averageTime = 0.0; //average time
+	double lowestTime = 0.0; //lowest time
+
+
+	//call functions
+	lowestTime = findLowest(finishTimes, SIZE); //store lowest time
+	averageTime = findAverage(finishTimes, SIZE); //store average time
+	//output results
+	cout << fixed << showpoint << setprecision(1); //set output format
+	cout << "The average time is: " << averageTime << endl; //output average time
+	cout << "The lowest time is: " << lowestTime << endl; //output lowest time
+
+	
+
+
+
    
     
     return 0;
